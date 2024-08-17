73e633cafadfd98d97de9884f9e258a740322e7e
Kevin Zúñiga <kevin.zun@gmail.com>
2024-08-16 23:27:51 -0500

    feat: move with right click

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,12 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-@export var destination: Node3D
+@export var dest_marker: Node3D
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+
+var destination := Vector3.ZERO
+var direction := Vector3.ZERO
+
+# Navigation based on Bramwell's tutorial (https://youtu.be/2W4JP48oZ8U?si=sewfGVQvYPRPc_lf)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +15,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var input := Vector3.ZERO
-	input.x = Input.get_axis("player_left", "player_right")
-	input.z = Input.get_axis("player_back", "player_forward")
-	apply_central_force(input * 1200 * delta)
+	pass
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("mouse_right_click"):
@@ -22,12 +25,24 @@ func _input(event: InputEvent) -> void:
 		if maybe_ray.is_empty():
 			return
 		var ray := maybe_ray[0]
-		print(ray)
-		destination.position.x = ray.x
-		destination.position.z = ray.z
-		destination.visible = true
+		dest_marker.position.x = ray.x
+		dest_marker.position.z = ray.z
+		dest_marker.visible = true
+		navigation_agent_3d.target_position = ray
 
-# TODO: this doesn't work, returns a wrong position
+func _physics_process(delta: float) -> void:
+	var dest := navigation_agent_3d.get_next_path_position()
+	var local_dest := dest - global_position
+	if local_dest.length() < 0.1:
+		dest_marker.visible = false # TODO: should probably emit an event here
+		return
+	var direction := local_dest.normalized()
+	velocity = direction * 5.0
+	look_at(global_position + velocity, Vector3.UP)
+	move_and_slide()
+
+# TODO: ~~this doesn't work, returns a wrong position~~
+#		It does work, but the flag transform needs to be set to top level, why?
 func raycast_screen_position(camera: Camera3D, pos: Vector2) -> Array[Vector3]:
 	const ray_length := 1000.0
 	var ray_from := camera.project_ray_origin(pos)
@@ -39,7 +54,6 @@ func raycast_screen_position(camera: Camera3D, pos: Vector2) -> Array[Vector3]:
 	ray_query.collide_with_areas = true
 	ray_query.collide_with_bodies = true
 	var result := space.intersect_ray(ray_query)
-	print(result)
 	if result.position:
 		return [result.position]
 	return []
